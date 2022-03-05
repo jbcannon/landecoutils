e59840727cc080c9bd42e9129f2eb33d91f4638a
Jeffery B Cannon <39226472+jbcannon@users.noreply.github.com>
2022-03-05 08:11:58 -0500

    Update clip_lidR_ctgs.R

--- R/clip_lidR_ctgs.R
+++ R/clip_lidR_ctgs.R
@@ -263,7 +263,7 @@ stitch_TLS_dir_to_LAS_tiles = function(ctg, out_dir, bnd, tile_size, n_cores, bu
   
   doParallel::registerDoParallel(parallel::makeCluster(n_cores))
   `%dopar%` = foreach::`%dopar%`
-  out = foreach(t=todo_list, .errorhandling = 'pass') %dopar% {
+  out = foreach(t=todo_list, .errorhandling = 'pass', .packages=c('sf', 'lidR')) %dopar% {
     # Load and display tile
     cat('\nloading tile', t, 'of', nrow(grid), '\n')
     tile = grid[t,]
