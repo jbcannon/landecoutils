f1a77479f21bbfdd2da0efd6b594e040e6dec83d
Jeffery B Cannon <39226472+jbcannon@users.noreply.github.com>
2022-03-13 15:08:44 -0400

    Update clip_lidR_ctgs.R

--- R/clip_lidR_ctgs.R
+++ R/clip_lidR_ctgs.R
@@ -234,7 +234,7 @@ stitch_TLS_dir_to_LAS_tiles = function(ctg, out_dir, bnd, tile_size, n_cores, bu
   for(t in 1:nrow(grid)){
     tile = grid[t,]
     ex = round(sf::st_bbox(tile))
-    out_las = paste0(out_dir, '/', ex[1], '_', ex[2], '.las')
+    out_las = paste0(out_dir, '/', ex[1], '_', ex[2], '.laz')
     if(!file.exists(out_las))  todo_list = c(todo_list, t)
   }
   
@@ -268,7 +268,7 @@ stitch_TLS_dir_to_LAS_tiles = function(ctg, out_dir, bnd, tile_size, n_cores, bu
     cat('\nloading tile', t, 'of', nrow(grid), '\n')
     tile = grid[t,]
     ex = sf::st_bbox(tile)
-    out_las = paste0(out_dir, '/', ex[1], '_', ex[2], '.las')
+    out_las = paste0(out_dir, '/', ex[1], '_', ex[2], '.laz')
     if(file.exists(out_las)) return(NULL)
     scans_to_load = scan_locations[sf::st_intersects(tile, scan_locations, sparse = FALSE),]
     if(nrow(scans_to_load)<1) return(NULL)
