f03a130212731b199af5221526e82fdf4dd005d1
Jeffery B Cannon <39226472+jbcannon@users.noreply.github.com>
2023-05-26 10:52:13 -0400

    Update clip_3d_voxel.R

--- R/clip_3d_voxel.R
+++ R/clip_3d_voxel.R
@@ -1,8 +1,8 @@
 #' Clip a LAS object to a volume of interest
-#' 
+#'
 #' This code is a 3-dimensional version of `lidR::clip_roi` which interprets
 #' a smaller las object as a volume of interest and clips a larger LAS object
-#' to that volume. Some segmentation algorithms result in lower resolution 
+#' to that volume. Some segmentation algorithms result in lower resolution
 #' LAS objects than the original files, so this function can be used to clip
 #' a low resolution segmentated LAS object and retrieve the volume specified from
 #' a higher resolution LAS.
@@ -10,27 +10,27 @@
 #' @param voi a `LAS` object often with lower resolution and extent from which
 #' to generate a volume to clip `las`
 #' @param `res` the resolution at which the `voi` is generated
-#' @examples 
+#' @examples
 #' library(landecoutils)
 #' library(lidR)
-#' 
+#'
 #' # Large full resolution LAS object
 #' full_las = readTLSLAS('path/to/highres.las')
-#' 
+#'
 #' # smaller lower resolution LAS object
 #' voi = readTLSLAS('path/to/lowres.las')
-#' 
+#'
 #' # view point density an extent of each las
 #' nrow(full_las@data);extent(full_las)
 #' nrow(voi@data);extent(voi)
-#' 
+#'
 #' # Clip voi and view point density and extent
 #' high_res_voi = clip_voi(full_las, voi, res=0.5)
 #' nrow(high_res_voi@data); extent(high_res_voi)
 #' @export
 clip_voi = function(las, voi, res){
   vox = lidR::voxelize_points(voi, res)
-  bbox = as.numeric(voi@bbox)
+  bbox = as.numeric(sf::st_bbox(voi))
   high_res = lidR::filter_poi(las, X >= bbox[1] & X <= bbox[3] & Y >= bbox[2] & Y <= bbox[4])
   grid_temp = terra::rast(terra::ext(vox))
   terra::res(grid_temp) = c(res, res)
