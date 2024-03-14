ab9cc65e8982b62d36aa2a6aba20a473d15bcbf5
Jeffery B Cannon <39226472+jbcannon@users.noreply.github.com>
2024-03-14 14:07:47 -0400

    Update clip_lidR_ctgs.R

--- R/clip_lidR_ctgs.R
+++ R/clip_lidR_ctgs.R
@@ -382,7 +382,7 @@ stitch_TLS_dir_to_LAS_tiles = function(ctg, out_dir, bnd, tile_size, n_cores, bu
     #make sure las portion with highest NumberofReturns is listed first so bit count is set correctly
     whichMaxReturns = which.max(sapply(combined_las, function(x) max((x@data$NumberOfReturns))))
     n = c(whichMaxReturns, (1:length(combined_las))[-whichMaxReturns])
-    combined_las = do.call(rbind,combined_las[[n]])
+    combined_las = do.call(rbind,combined_las[n])
     combined_las@header@VLR = list()
     st_crs(combined_las) = proj
     #write tile to disk
